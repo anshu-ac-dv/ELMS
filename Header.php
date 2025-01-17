a2e65197eaf0bb59bfe3fa10c81e37a66f8aff54
Anshu Kumar <anshu.ac.dv@gmail.com>
2025-01-18 01:31:00 +0530

    Initial Commit

--- Header.php
+++ Header.php
@@ -4,16 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <style>
-        h2{
-            background-color: aqua;
-            text-align: center;
-            padding: 10px;
-        }
+        
     </style>
 </head>
 <body>
-    <h2>
-        Employee Leave Management System
-    </h2>
+    
 </body>
 </html>
--- index.php
+++ index.php
@@ -3,35 +3,33 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <title>Employee Leave Manegement System</title>
     <link rel="stylesheet" href="style.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
   <body>
-    <?php
-        include "Header.php";
-    ?>
+    <h1>
+        Employee Leave Management System
+    </h1>
     <div class="card-container">
         <!-- Card 1 -->
         <div class="card">
-            <img src="https://via.placeholder.com/300x150" alt="Card Image" class="card-image">
+            <img src="emp.jpg" alt="Card Image" class="card-image">
             <div class="card-content">
-                <h3 class="card-title">Card Title 1</h3>
-                <p class="card-text">This is a description for the first card. Add relevant information here.</p>
-                <button class="card-button">Learn More</button>
+                <h3 class="card-title">Employee Login</h3>
+                <p class="card-text">For Employee login click below button of in this card.</p>
+                <button class="card-button">Login Now</button>
             </div>
         </div>
         
         <!-- Card 2 -->
         <div class="card">
-            <img src="https://via.placeholder.com/300x150" alt="Card Image" class="card-image">
+            <img src="hr.jpg" alt="Card Image" class="card-image">
             <div class="card-content">
-                <h3 class="card-title">Card Title 2</h3>
-                <p class="card-text">This is a description for the second card. Add relevant information here.</p>
-                <button class="card-button">Learn More</button>
+                <h3 class="card-title">HR Login</h3>
+                <p class="card-text">For Human Resourse login click below button of in this card.</p>
+                <button class="card-button">Login Now</button>
             </div>
         </div>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
   </body>
 </html>
