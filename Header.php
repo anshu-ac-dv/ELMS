036a6b6fc905432ee72931a94e529c820484f2fd
Anshu Kumar <anshu.ac.dv@gmail.com>
2025-01-17 19:49:54 +0530

    Initial Commit

--- Header.php
+++ Header.php
@@ -1,5 +1,19 @@
-<nav class="navbar bg-body-tertiary">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="index.php">Employee Leave Management System</a>
-        </div>
-    </nav>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        h2{
+            background-color: aqua;
+            text-align: center;
+            padding: 10px;
+        }
+    </style>
+</head>
+<body>
+    <h2>
+        Employee Leave Management System
+    </h2>
+</body>
+</html>
