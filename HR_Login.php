8853b479e528850ac498419dc4a0bf4d9dc60062
Anshu Kumar <anshu.ac.dv@gmail.com>
2025-01-27 00:09:26 +0530

    Initial Commit

--- HR_Login.php
+++ HR_Login.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="Style.css">
+    <link rel="stylesheet" href="Style2.css">
     <title>HR Login - Employee Leave Management System</title>
 </head>
 <body>
@@ -12,6 +12,14 @@
         include 'Header.php';
     ?>
 
-    
+    <div class="login-container">
+        <h1>HR Login</h1>
+        <form action="#" method="POST">
+        <input type="text" name="employee-id" placeholder="Employee ID" required>
+        <input type="password" name="password" placeholder="Password" required>
+        <button type="submit">Login</button>
+        </form>
+        <p>Forgot your password? <a href="#">Click here</a></p>
+    </div>
 </body>
 </html>
